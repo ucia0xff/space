f981f81159b1e58231353d118ac2130ef365407e
ucia0xff <ucia0xff@gmail.com>
2018-11-01 13:38:50 +0800

    init

---
+++ app/src/main/java/io/github/ucia0xff/space/MainActivity.java
@@ -0,0 +1,16 @@
+package io.github.ucia0xff.space;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    }
+}
